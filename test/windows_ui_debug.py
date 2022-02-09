5244673b81e6743704a02759987f0e391c24140b
Nancy <1187535435@qq.com>
2022-02-09 11:08:36 +0800

    add selenium auto scripts

--- test/windows_ui_debug.py
+++ test/windows_ui_debug.py
@@ -15,8 +15,8 @@ class Iselenium(unittest.TestCase):
     #读入配置文件
     def get_config(self):
         config=configparser.ConfigParser()
-        config.read(os.path.join(os.environ['HOME'],'iselenum.ini'))#linux中
-        # config.read(os.path.abspath('..') + '\iselenium.ini')#windows中
+        # config.read(os.path.join(os.environ['HOME'],'iselenum.ini'))#linux中
+        config.read(os.path.abspath('..') + '\iselenium.ini')#windows中
 
         return config
 
@@ -28,17 +28,17 @@ class Iselenium(unittest.TestCase):
         config=self.get_config()
 
         # 控制是否采用无界面形式运行自动化测试
-        try:
-            using_headless=os.environ["using_headless"]
-        except KeyError:
-            using_headless=None
-            print('没有配置环境变量 using_headless,按照有界面方式运行自动化测试')
+        # try:
+        #     using_headless=os.environ["using_headless"]
+        # except KeyError:
+        #     using_headless=None
+        #     print('没有配置环境变量 using_headless,按照有界面方式运行自动化测试')
 
         chrome_options=Options()
 
-        if using_headless is not None and using_headless.lower()=='true':
-            print("使用无界面方式运行")
-            chrome_options.add_argument("--headless")
+        # if using_headless is not None and using_headless.lower()=='true':
+        #     print("使用无界面方式运行")
+        #     chrome_options.add_argument("--headless")
 
 
 
